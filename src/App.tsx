f675278870118758d2eba71b4b117450841a230c
Mustafa ÖZTÜRK <bay.mustafa.37@hotmail.com>
2022-06-25 18:36:57 +0300

    Add toast and other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,78 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 import { ITodoItem } from "types";
 import TodoService from "services";
 
+import Check from "components/check";
+import ListItem from "components/list-item";
+import TaskInput from "components/task-input";
+
 const App: FC = () => {
   const [todoItems, setTodoItems] = useState<ITodoItem[]>([]);
 
-  const addFunction = async () => {
+  // All todos fetched from server
+  const getAllTodos = async () => {
     try {
-      const todo = await TodoService.Add("deneme todo");
-      console.log(todo);
+      const todos = await TodoService.GetAll();
+
+      if (todos.status) {
+        //  todos data sort by id
+        const sortedTodos = todos.data.sort(
+          (a: ITodoItem, b: ITodoItem) => b.id - a.id
+        );
+        setTodoItems(sortedTodos);
+      } else {
+        setTodoItems([]);
+      }
     } catch (err) {
       console.log(err);
     }
   };
 
-  return <div className="content">A</div>;
+  const addFunction = async (text: string) => {
+    try {
+      text = text.trim();
+      const todo = await TodoService.Add(text);
+      console.log(todo);
+      toast.success("Successfully toasted!");
+      getAllTodos();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Page load call getAllTodos
+  useEffect(() => {
+    getAllTodos();
+  }, []);
+
+  return (
+    <div className="content">
+      <div className="head">
+        <h1>All Tasks</h1>
+        <span>( 0 / 0 )</span>
+      </div>
+
+      <TaskInput onAdd={addFunction} />
+
+      <ul className="list">
+        {todoItems.map((item) => (
+          <ListItem
+            key={item.id}
+            item={item}
+            onStatusChange={() => {
+              console.log("status change");
+            }}
+            onDelete={() => {
+              console.log("delete");
+            }}
+          />
+        ))}
+      </ul>
+      <Toaster position="top-right" reverseOrder={false} />
+    </div>
+  );
 };
 
 export default App;
